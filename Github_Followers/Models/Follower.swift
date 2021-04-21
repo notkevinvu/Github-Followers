f87adae433251e65902f9e16ddfd01d6ae1513d2
Kevin Vu <kevinvu59@gmail.com>
2021-04-20 23:54:42 -0400

    Revert small access control levels in the follower model, as it didn't fix a libquic failed error which is a simulator error.

--- Github_Followers/Models/Follower.swift
+++ Github_Followers/Models/Follower.swift
@@ -10,11 +10,11 @@ import Foundation
 struct Follower: Codable {
     // both are non-optional as the github api will always return a non-nil/null
     // value for these properties in the response JSON
-    public var login: String
+    var login: String
     // avatarUrl is actually avatar_url in JSON response. We can use the
     // .convertFromSnakeCase keyDecodingStrategy when creating a Decoder object
     // to handle these cases
-    public var avatarUrl: String
+    var avatarUrl: String
 }
 
 

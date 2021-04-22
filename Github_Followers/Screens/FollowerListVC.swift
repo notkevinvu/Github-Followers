f03dacc8d0f1c6c7b5d808462864524bb41b97c5
Kevin Vu <kevinvu59@gmail.com>
2021-04-22 16:30:29 -0400

    Add collection view and configure its layout in FollowerListVC.

--- Github_Followers/Screens/FollowerListVC.swift
+++ Github_Followers/Screens/FollowerListVC.swift
@@ -9,17 +9,19 @@ import UIKit
 
 final class FollowerListVC: UIViewController {
     
+    // MARK: - UI Properties
+    private var collectionView: UICollectionView!
     
     // MARK: - Properties
+    // set in getFollowersOnLoad()
     var username: String!
     
     
     // MARK: - View lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .systemBackground
-        navigationController?.navigationBar.prefersLargeTitles = true
-        
+        configureViewController()
+        configureCollectionView()
         getFollowersOnLoad()
     }
     
@@ -33,9 +35,18 @@ final class FollowerListVC: UIViewController {
 }
 
 
-extension FollowerListVC {
-    // MARK: - Network methods
-    private func getFollowersOnLoad() {
+// MARK: - View controller methods
+private extension FollowerListVC {
+    func configureViewController() {
+        view.backgroundColor = .systemBackground
+        navigationController?.navigationBar.prefersLargeTitles = true
+    }
+}
+
+
+// MARK: - Network methods
+private extension FollowerListVC {
+    func getFollowersOnLoad() {
         // libquic failed error is apparently a simulator error - ignore,
         // it does not affect behavior in any apparent way
         NetworkManager.shared.getFollowers(for: username, page: 1) { [weak self] (result) in
@@ -50,5 +61,46 @@ extension FollowerListVC {
                     self.presentGFAlertOnMainThread(title: "Bad stuff happened", message: errorMessage.rawValue, buttonTitle: "Ok")
             }
         }
+    }
+}
+
+
+// MARK: - Collection view configuration
+private extension FollowerListVC {
+    func createThreeColumnCompositionalLayout() -> UICollectionViewCompositionalLayout {
+        let width = view.bounds.width
+        let padding: CGFloat = 12
+        let minimumItemSpacing: CGFloat = 10
+        // width minus 2x the other variables since we have 3 items per column
+        let availableWidth = width - (padding * 2) - (minimumItemSpacing * 2)
+        let itemWidth = availableWidth / 3
+        
+        let itemSize = NSCollectionLayoutSize(
+            widthDimension: .absolute(itemWidth),
+            heightDimension: .fractionalHeight(1.0))
+        let item = NSCollectionLayoutItem(layoutSize: itemSize)
+        let insets: CGFloat = 10
+        item.contentInsets = NSDirectionalEdgeInsets(top: insets, leading: insets, bottom: 0, trailing: insets)
+        
+        // height is the width + 40 so we get a more rectangular item. This is
+        // because we have both the imageView and the name label, so if we
+        // want a roughly square image view, we need extra space for the label
+        let groupSize = NSCollectionLayoutSize(
+            widthDimension: .fractionalWidth(1.0),
+            heightDimension: .absolute(itemWidth + 40))
+        let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize, subitems: [item])
+        
+        let section = NSCollectionLayoutSection(group: group)
+        let layout = UICollectionViewCompositionalLayout(section: section)
+        
+        return layout
+    }
+    
+    
+    func configureCollectionView() {
+        collectionView = UICollectionView(frame: view.bounds, collectionViewLayout: createThreeColumnCompositionalLayout())
+        collectionView.register(FollowerCell.self, forCellWithReuseIdentifier: FollowerCell.identifier)
+        view.addSubview(collectionView)
+        collectionView.backgroundColor = .systemPink
     }
 }

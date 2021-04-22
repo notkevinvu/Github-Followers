9cf9b4984d53b4d704585955e210a3b694a603f3
Kevin Vu <kevinvu59@gmail.com>
2021-04-22 16:53:21 -0400

    Add diffable data source custom class for the follower list collection view (haven't connected it to follower list vc yet)

---
+++ Github_Followers/Models/Data Sources/FollowerListDiffableDataSource.swift
@@ -0,0 +1,64 @@
+//
+//  FollowerListDiffableDataSource.swift
+//  Github_Followers
+//
+//  Created by Kevin Vu on 4/22/21.
+//
+
+import UIKit
+
+final class FollowerListDiffableDataSource: NSObject {
+    
+    // MARK: - Properties
+    typealias DataSource = UICollectionViewDiffableDataSource<Section, Follower>
+    var dataSource: DataSource!
+    
+    typealias Snapshot = NSDiffableDataSourceSnapshot<Section, Follower>
+    
+    
+    // MARK: - Init
+    init(collectionView: UICollectionView) {
+        super.init()
+        dataSource = makeDataSource(for: collectionView)
+    }
+}
+
+
+// MARK: - Utility methods
+extension FollowerListDiffableDataSource {
+    func applySnapshot(forFollowers followers: [Follower]) {
+        var snapshot = Snapshot()
+        snapshot.appendSections([.main])
+        snapshot.appendItems(followers)
+        
+        dataSource.apply(snapshot)
+    }
+    
+    func remove(_ follower: Follower, animate: Bool = true) {
+        var snapshot = dataSource.snapshot()
+        snapshot.deleteItems([follower])
+        dataSource.apply(snapshot, animatingDifferences: animate)
+    }
+}
+
+
+// MARK: - Setup methods
+private extension FollowerListDiffableDataSource {
+    func makeDataSource(for collectionView: UICollectionView) -> DataSource {
+        return DataSource(collectionView: collectionView) { (collectionView, indexPath, follower) -> UICollectionViewCell? in
+            
+            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: FollowerCell.reuseID, for: indexPath) as? FollowerCell
+            cell?.set(follower: follower)
+            
+            return cell
+        }
+    }
+}
+
+
+// MARK: - Sections
+extension FollowerListDiffableDataSource {
+    enum Section {
+        case main
+    }
+}

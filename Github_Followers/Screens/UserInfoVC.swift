80bf0109aaea426d5d1c5a29b65a302570af0f05
Kevin Vu <kevinvu59@gmail.com>
2021-04-27 21:57:13 -0400

    Add network call method to get the corresponding user

--- Github_Followers/Screens/UserInfoVC.swift
+++ Github_Followers/Screens/UserInfoVC.swift
@@ -35,6 +35,8 @@ final class UserInfoVC: UIViewController {
         super.viewDidLoad()
         configureViewController()
         configureNavController()
+        
+        getUser()
     }
     
 }
@@ -50,6 +52,23 @@ private extension UserInfoVC {
     func configureNavController() {
         let doneButton = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(dismissVC))
         navigationItem.rightBarButtonItem = doneButton
+    }
+}
+
+
+// MARK: - Network methods
+private extension UserInfoVC {
+    func getUser() {
+        NetworkManager.shared.getUser(for: username) { [weak self] result in
+            guard let self = self else { return }
+            
+            switch result {
+                case .success(let user):
+                    print(user)
+                case .failure(let error):
+                    self.presentGFAlertOnMainThread(title: "Something went wrong", message: error.rawValue, buttonTitle: "Ok")
+            }
+        }
     }
 }
 

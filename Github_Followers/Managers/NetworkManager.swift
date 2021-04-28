a5841e00152e7bef8938a36c223dbdcb989731c8
Kevin Vu <kevinvu59@gmail.com>
2021-04-27 21:56:12 -0400

    Add another network call for getting a specific user's info

--- Github_Followers/Managers/NetworkManager.swift
+++ Github_Followers/Managers/NetworkManager.swift
@@ -18,6 +18,7 @@ final class NetworkManager {
     private init() {}
     
     
+    // MARK: - Get followers
     public func getFollowers(for username: String, page: Int, completion: @escaping (Result<[Follower], GFError>) -> Void) {
         let endpoint = baseURL + "users/\(username)/followers?per_page=\(numberOfFollowersPerPage)&page=\(page)"
         
@@ -48,6 +49,46 @@ final class NetworkManager {
                 
                 let followers = try decoder.decode([Follower].self, from: data)
                 completion(.success(followers))
+            } catch {
+                completion(.failure(.invalidData))
+            }
+        }
+        
+        task.resume()
+    }
+    
+    
+    // MARK: - Get user
+    public func getUser(for username: String, completion: @escaping (Result<User, GFError>) -> Void) {
+        let endpoint = baseURL + "users/\(username)"
+        
+        guard let url = URL(string: endpoint) else {
+            completion(.failure(.invalidUsername))
+            return
+        }
+        
+        let task = URLSession.shared.dataTask(with: url) { (data, response, error) in
+            guard error == nil else {
+                completion(.failure(.unableToComplete))
+                return
+            }
+            
+            guard let response = response as? HTTPURLResponse, response.statusCode == 200 else {
+                completion(.failure(.invalidResponse))
+                return
+            }
+            
+            guard let data = data else {
+                completion(.failure(.invalidData))
+                return
+            }
+            
+            do {
+                let decoder = JSONDecoder()
+                decoder.keyDecodingStrategy = .convertFromSnakeCase
+                
+                let user = try decoder.decode(User.self, from: data)
+                completion(.success(user))
             } catch {
                 completion(.failure(.invalidData))
             }

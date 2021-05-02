37fe05288678abbe6538a65c1ab0bba0687f868e
Kevin Vu <kevinvu59@gmail.com>
2021-05-02 15:31:19 -0400

    Create a header view controller to be used for the UserInfoVC and set up internal subviews

---
+++ Github_Followers/Reusable Views/View controllers/GFUserInfoHeaderVC.swift
@@ -0,0 +1,122 @@
+//
+//  GFUserInfoHeaderVC.swift
+//  Github_Followers
+//
+//  Created by Kevin Vu on 5/2/21.
+//
+
+import UIKit
+
+final class GFUserInfoHeaderVC: UIViewController {
+    
+    // MARK: - UI Properties
+    let avatarImageView = GFAvatarImageView(frame: .zero)
+    let usernameLabel = GFTitleLabel(textAlignment: .left, fontSize: 34)
+    let nameLabel = GFSecondaryTitleLabel(fontSize: 18)
+    
+    let locationImageView: UIImageView = {
+        let imgView = UIImageView(frame: .zero)
+        imgView.translatesAutoresizingMaskIntoConstraints = false
+        return imgView
+    }()
+    let locationLabel = GFSecondaryTitleLabel(fontSize: 18)
+    
+    let bioLabel = GFBodyLabel(textAlignment: .left)
+    
+    // MARK: - Properties
+    var user: User
+    
+    
+    // MARK: - Init
+    init(user: User) {
+        self.user = user
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    
+    // MARK: - View lifecycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        addSubviews()
+        layoutUI()
+        configureUIElements()
+    }
+    
+}
+
+
+// MARK: - Subview configuration
+extension GFUserInfoHeaderVC {
+    private func configureUIElements() {
+        avatarImageView.downloadImage(from: user.avatarUrl)
+        usernameLabel.text = user.login
+        nameLabel.text = user.name ?? ""
+        locationLabel.text = user.location ?? "No location"
+        bioLabel.text = user.bio ?? "No bio available"
+        bioLabel.numberOfLines = 3
+        
+        locationImageView.image = UIImage(systemName: SFSymbols.location)
+        // same color as secondary label (e.g. location label)
+        locationImageView.tintColor = .secondaryLabel
+    }
+    
+    
+    private func addSubviews() {
+        view.addSubview(avatarImageView)
+        view.addSubview(usernameLabel)
+        view.addSubview(nameLabel)
+        view.addSubview(locationImageView)
+        view.addSubview(locationLabel)
+        view.addSubview(bioLabel)
+    }
+    
+    
+    private func layoutUI() {
+        let padding: CGFloat = 20
+        let textImagePadding: CGFloat = 12
+        
+        NSLayoutConstraint.activate([
+            avatarImageView.topAnchor.constraint(equalTo: view.topAnchor, constant: padding),
+            avatarImageView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: padding),
+            // trial and error
+            avatarImageView.widthAnchor.constraint(equalToConstant: 90),
+            avatarImageView.heightAnchor.constraint(equalToConstant: 90),
+            
+            usernameLabel.topAnchor.constraint(equalTo: avatarImageView.topAnchor),
+            usernameLabel.leadingAnchor.constraint(equalTo: avatarImageView.trailingAnchor, constant: textImagePadding),
+            usernameLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -padding),
+            // label height slightly bigger than the font size
+            usernameLabel.heightAnchor.constraint(equalToConstant: 38),
+            
+            // additional constant padding to make text visually centered,
+            // rather than just the label
+            nameLabel.centerYAnchor.constraint(equalTo: avatarImageView.centerYAnchor, constant: 8),
+            nameLabel.leadingAnchor.constraint(equalTo: usernameLabel.leadingAnchor),
+            nameLabel.trailingAnchor.constraint(equalTo: usernameLabel.trailingAnchor),
+            // again, height slightly bigger than font size
+            nameLabel.heightAnchor.constraint(equalToConstant: 20),
+            
+            locationImageView.bottomAnchor.constraint(equalTo: avatarImageView.bottomAnchor),
+            locationImageView.leadingAnchor.constraint(equalTo: nameLabel.leadingAnchor),
+            locationImageView.widthAnchor.constraint(equalToConstant: 20),
+            locationImageView.heightAnchor.constraint(equalToConstant: 20),
+            
+            locationLabel.centerYAnchor.constraint(equalTo: locationImageView.centerYAnchor),
+            locationLabel.leadingAnchor.constraint(equalTo: locationImageView.trailingAnchor, constant: 5),
+            locationLabel.trailingAnchor.constraint(equalTo: nameLabel.trailingAnchor),
+            locationLabel.heightAnchor.constraint(equalToConstant: 20),
+            
+            bioLabel.topAnchor.constraint(equalTo: avatarImageView.bottomAnchor, constant: textImagePadding),
+            bioLabel.leadingAnchor.constraint(equalTo: avatarImageView.leadingAnchor),
+            bioLabel.trailingAnchor.constraint(equalTo: nameLabel.trailingAnchor),
+            // want 3 lines max and 60 height gives us roughly that
+            bioLabel.heightAnchor.constraint(equalToConstant: 60)
+        ])
+    }
+    
+}

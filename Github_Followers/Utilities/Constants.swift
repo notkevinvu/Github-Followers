cec66c3be6fe53f2ece635682b7a3c1adf93fc63
Kevin Vu <kevinvu59@gmail.com>
2021-05-02 15:29:27 -0400

    Add a constants file, currently holding some sfsymbol string keys for ease of access

---
+++ Github_Followers/Utilities/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  Github_Followers
+//
+//  Created by Kevin Vu on 5/2/21.
+//
+
+import Foundation
+
+enum SFSymbols {
+    static let location = "mappin.and.ellipse"
+}

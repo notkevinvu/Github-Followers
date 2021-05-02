c27f4ba977ce81c5e0a5db220e04bcf34fe24a43
Kevin Vu <kevinvu59@gmail.com>
2021-05-02 15:27:29 -0400

    Add another reusable title label for secondary titles specifically

---
+++ Github_Followers/Reusable Views/Labels/GFSecondaryTitleLabel.swift
@@ -0,0 +1,45 @@
+//
+//  GFSecondaryTitleLabel.swift
+//  Github_Followers
+//
+//  Created by Kevin Vu on 5/2/21.
+//
+
+import UIKit
+
+// prefer to create an entirely new reusable label specifically for secondary
+// titles rather than create one label that is very flexible. This is for
+// ease of use (i.e. if we ever need to add a secondary title label, we just
+// use this class instead of having to pass in numerous parameters
+final class GFSecondaryTitleLabel: UILabel {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    
+    init(fontSize: CGFloat) {
+        super.init(frame: .zero)
+        font = UIFont.systemFont(ofSize: fontSize, weight: .medium)
+        configure()
+    }
+}
+
+
+// MARK: - Configure methods
+private extension GFSecondaryTitleLabel {
+    func configure() {
+        textColor = .secondaryLabel
+        adjustsFontSizeToFitWidth = true
+        minimumScaleFactor = 0.90
+        // saving configuration of numberOfLines for when we instantiate
+        lineBreakMode = .byTruncatingTail
+        translatesAutoresizingMaskIntoConstraints = false
+    }
+}

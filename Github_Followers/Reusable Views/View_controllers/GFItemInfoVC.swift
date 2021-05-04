6aa58b26b510d7ee153b9615c3499121c0f61719
Kevin Vu <kevinvu59@gmail.com>
2021-05-04 01:03:49 -0400

    Add superclass vc for the two VCs we will use in the user info vc

---
+++ Github_Followers/Reusable Views/View_controllers/GFItemInfoVC.swift
@@ -0,0 +1,68 @@
+//
+//  GFItemInfoVC.swift
+//  Github_Followers
+//
+//  Created by Kevin Vu on 5/4/21.
+//
+
+import UIKit
+
+// used as superclass
+class GFItemInfoVC: UIViewController {
+    
+    // MARK: - UI Properties
+    let stackView = UIStackView()
+    let itemInfoViewOne = GFItemInfoView()
+    let itemInfoViewTwo = GFItemInfoView()
+    let actionButton = GFButton()
+    
+    
+    // MARK: - View lifecycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureBackgroundView()
+        configureStackView()
+        layoutUI()
+    }
+    
+}
+
+
+// MARK: - Configuration
+extension GFItemInfoVC {
+    private func configureBackgroundView() {
+        view.layer.cornerRadius = 18
+        view.backgroundColor = .secondarySystemBackground
+    }
+    
+    
+    private func configureStackView() {
+        stackView.axis = .horizontal
+        stackView.distribution = .equalSpacing
+        
+        stackView.addArrangedSubview(itemInfoViewOne)
+        stackView.addArrangedSubview(itemInfoViewTwo)
+    }
+    
+    
+    private func layoutUI() {
+        view.addSubview(stackView)
+        view.addSubview(actionButton)
+        
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        let padding: CGFloat = 20
+        
+        NSLayoutConstraint.activate([
+            stackView.topAnchor.constraint(equalTo: view.topAnchor, constant: padding),
+            stackView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: padding),
+            stackView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -padding),
+            stackView.heightAnchor.constraint(equalToConstant: 50),
+            
+            actionButton.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -padding),
+            actionButton.leadingAnchor.constraint(equalTo: stackView.leadingAnchor),
+            actionButton.trailingAnchor.constraint(equalTo: stackView.trailingAnchor),
+            // human interface guideline for touch target
+            actionButton.heightAnchor.constraint(equalToConstant: 44)
+        ])
+    }
+}

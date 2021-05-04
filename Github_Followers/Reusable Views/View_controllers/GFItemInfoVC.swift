120fe7e4e4ac0debe6a4fbdab752292852c9bad9
Kevin Vu <kevinvu59@gmail.com>
2021-05-04 17:58:34 -0400

    Add user property and corresponding init to access user properties for UI

--- Github_Followers/Reusable Views/View_controllers/GFItemInfoVC.swift
+++ Github_Followers/Reusable Views/View_controllers/GFItemInfoVC.swift
@@ -17,6 +17,22 @@ class GFItemInfoVC: UIViewController {
     let actionButton = GFButton()
     
     
+    // MARK: - Properties
+    let user: User
+    
+    
+    // MARK: - Init
+    init(user: User) {
+        self.user = user
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    
     // MARK: - View lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()

f170853c37cdc9f5b19aed50ca6f7493ed723978
Kevin Vu <kevinvu59@gmail.com>
2021-05-07 03:53:41 -0400

    Add basic retrieval from persistence manager to verify favorites.

--- Github_Followers/Screens/FavoritesListVC.swift
+++ Github_Followers/Screens/FavoritesListVC.swift
@@ -13,6 +13,15 @@ final class FavoritesListVC: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
+        
+        PersistenceManager.retrieveFavorites { result in
+            switch result {
+                case .success(let favorites):
+                    print(favorites)
+                case .failure(let error):
+                    break
+            }
+        }
     }
     
 }
